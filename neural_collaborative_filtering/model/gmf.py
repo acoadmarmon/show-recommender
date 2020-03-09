fa76260beb81f9048559bdc95b2d4ba83d956c09
Andrew Marmon <andrew.marmon@slalom.com>
2020-03-09 17:55:12 -0400

    refactored to utilize torch.nn

--- neural_collaborative_filtering/model/gmf.py
+++ neural_collaborative_filtering/model/gmf.py
@@ -12,11 +12,11 @@ class GMF(BaseModel):
         self.num_items = config['num_items']
         self.latent_dim = config['latent_dim']
 
-        self.embedding_user = torch.nn.Embedding(num_embeddings=self.num_users, embedding_dim=self.latent_dim)
-        self.embedding_item = torch.nn.Embedding(num_embeddings=self.num_items, embedding_dim=self.latent_dim)
+        self.embedding_user = nn.Embedding(num_embeddings=self.num_users, embedding_dim=self.latent_dim)
+        self.embedding_item = nn.Embedding(num_embeddings=self.num_items, embedding_dim=self.latent_dim)
 
-        self.affine_output = torch.nn.Linear(in_features=self.latent_dim, out_features=1)
-        self.logistic = torch.nn.Sigmoid()
+        self.affine_output = nn.Linear(in_features=self.latent_dim, out_features=1)
+        self.logistic = nn.Sigmoid()
 
     def forward(self, user_indices, item_indices):
         user_embedding = self.embedding_user(user_indices)
