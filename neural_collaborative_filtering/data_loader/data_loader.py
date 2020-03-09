16d7a6cf4a1c6eb18c98906c2b8369c097edf842
Andrew Marmon <andrew.marmon@slalom.com>
2020-03-09 17:05:59 -0400

    implemented pytorch model and data loaders, conducting training now.

---
+++ neural_collaborative_filtering/data_loader/data_loader.py
@@ -0,0 +1,102 @@
+import torch
+import random
+import pandas as pd
+from copy import deepcopy
+from torch.utils.data import DataLoader, Dataset
+import numpy as np
+
+class UserItemRatingDataset(Dataset):
+    """Wrapper, convert <user, item, rating> Tensor into Pytorch Dataset"""
+    def __init__(self, user_tensor, item_tensor, target_tensor):
+        """
+        args:
+            target_tensor: torch.Tensor, the corresponding rating for <user, item> pair
+        """
+        self.user_tensor = user_tensor
+        self.item_tensor = item_tensor
+        self.target_tensor = target_tensor
+
+    def __getitem__(self, index):
+        return self.user_tensor[index], self.item_tensor[index], self.target_tensor[index]
+
+    def __len__(self):
+        return self.user_tensor.size(0)
+
+
+class SampleGenerator(object):
+    """Construct dataset for NCF"""
+
+    def __init__(self, ratings):
+        """
+        args:
+            ratings: pd.DataFrame, which contains 4 columns = user_id, item_id, rating
+        """
+        assert 'user_id' in ratings.columns
+        assert 'item_id' in ratings.columns
+        assert 'rating' in ratings.columns
+
+        self.ratings = ratings
+        self.preprocess_ratings = self._binarize(ratings)
+        self.user_pool = set(self.ratings['user_id'].unique())
+        self.item_pool = set(self.ratings['item_id'].unique())
+
+        # create negative item samples for NCF learning
+        self.preprocess_ratings = self._sample_negative(self.preprocess_ratings)
+        self.train_ratings, self.test_ratings = self._split_loo(self.preprocess_ratings)
+
+    def _normalize(self, ratings):
+        """normalize into [0, 1] from [0, max_rating], explicit feedback"""
+        ratings = deepcopy(ratings)
+        max_rating = ratings.rating.max()
+        ratings['rating'] = ratings.rating * 1.0 / max_rating
+        return ratings
+
+    def _binarize(self, ratings):
+        """binarize into 0 or 1, imlicit feedback"""
+        #ratings = deepcopy(ratings)
+        #ratings['rating'][(ratings['rating'] > 0.0) & (ratings['rating'] < 6.0)] = -2.0
+        ratings['rating'][ratings['rating'] >= -1.0] = 1.0
+        return ratings
+
+    def _split_loo(self, ratings):
+        """leave one out train/test split """
+
+        ratings['rank'] = np.random.randint(1, 100, ratings.shape[0])
+        ratings['rank_latest'] = ratings.groupby(['user_id'])['rank'].rank(method='first', ascending=False)
+        test = ratings[ratings['rank_latest'] == 1]
+        train = ratings[ratings['rank_latest'] > 1]
+        assert train['user_id'].nunique() == test['user_id'].nunique()
+        return train[['user_id', 'item_id', 'rating']], test[['user_id', 'item_id', 'rating']]
+
+    def _sample_negative(self, ratings):
+        """return all negative items & 100 sampled negative items"""
+        interact_status = pd.DataFrame(ratings.groupby('user_id')['item_id'].apply(set)).rename(
+            columns={'item_id': 'interacted_items'})
+        interact_status['negative_items'] = interact_status['interacted_items'].apply(lambda x: self.item_pool - x)
+        interact_status['negative_samples'] = interact_status['negative_items'].apply(lambda x: random.sample(x, 10))
+        interacted_with = interact_status.explode('negative_samples')
+        interacted_with['rating'] = 0.0
+        interacted_with = interacted_with.drop(['interacted_items', 'negative_items'], axis=1)
+        interacted_with = interacted_with.rename(columns={'negative_samples': 'item_id'}).reset_index()
+        return pd.concat([interacted_with, ratings])
+
+    def instance_a_train_loader(self, batch_size):
+        """instance train loader for one training epoch"""
+        dataset = UserItemRatingDataset(user_tensor=torch.LongTensor(list(self.train_ratings['user_id'])),
+                                        item_tensor=torch.LongTensor(list(self.train_ratings['item_id'])),
+                                        target_tensor=torch.FloatTensor(list(self.train_ratings['rating'])))
+        return DataLoader(dataset, batch_size=batch_size, shuffle=True)
+
+    @property
+    def evaluate_data(self):
+        """create evaluate data"""
+        interact_status = pd.DataFrame(self.test_ratings.groupby('user_id')['item_id'].apply(set)).rename(
+            columns={'item_id': 'interacted_items'})
+        interact_status['negative_items'] = interact_status['interacted_items'].apply(lambda x: self.item_pool - x)
+        interact_status['negative_samples'] = interact_status['negative_items'].apply(lambda x: random.sample(x, 15))
+        interacted_with = interact_status.explode('negative_samples')
+        interacted_with['rating'] = 0.0
+        interacted_with = interacted_with.drop(['interacted_items', 'negative_items'], axis=1)
+        interacted_with = interacted_with.rename(columns={'negative_samples': 'item_id'}).reset_index()
+        return [torch.LongTensor(list(self.test_ratings['user_id'])), torch.LongTensor(list(self.test_ratings['item_id'])), torch.LongTensor(list(interacted_with['user_id'])),
+                torch.LongTensor(list(interacted_with['item_id']))]
---
+++ neural_collaborative_filtering/train.py
@@ -0,0 +1,78 @@
+import pandas as pd
+import numpy as np
+from data_loader.data_loader import SampleGenerator
+from trainer.trainer import Trainer
+from model.gmf import GMF
+
+gmf_config = {'alias': 'gmf_factor8neg4-implict',
+              'num_epoch': 200,
+              'batch_size': 1024,
+              # 'optimizer': 'sgd',
+              # 'sgd_lr': 1e-3,
+              # 'sgd_momentum': 0.9,
+              # 'optimizer': 'rmsprop',
+              # 'rmsprop_lr': 1e-3,
+              # 'rmsprop_alpha': 0.99,
+              # 'rmsprop_momentum': 0,
+              'optimizer': 'adam',
+              'adam_lr': 1e-3,
+              'num_users': 73517,
+              'num_items': 34520,
+              'latent_dim': 32,
+              'num_negative': 4,
+              'l2_regularization': 0, # 0.01
+              'use_cuda': False,
+              'device_id': 0,
+              'model_dir':'checkpoints/{}_Epoch{}_HR{:.4f}_NDCG{:.4f}.model'}
+
+mlp_config = {'alias': 'mlp_factor8neg4_bz256_166432168_pretrain_reg_0.0000001',
+              'num_epoch': 200,
+              'batch_size': 256,  # 1024,
+              'optimizer': 'adam',
+              'adam_lr': 1e-3,
+              'num_users': 73515,
+              'num_items': 11200,
+              'latent_dim': 32,
+              'num_negative': 4,
+              'layers': [16,64,32,16,8],  # layers[0] is the concat of latent user vector & latent item vector
+              'l2_regularization': 0.0000001,  # MLP model is sensitive to hyper params
+              'use_cuda': True,
+              'device_id': 7,
+              'pretrain': True,
+              'pretrain_mf': 'checkpoints/{}'.format('gmf_factor8neg4_Epoch100_HR0.6391_NDCG0.2852.model'),
+              'model_dir':'checkpoints/{}_Epoch{}_HR{:.4f}_NDCG{:.4f}.model'}
+
+neumf_config = {'alias': 'pretrain_neumf_factor8neg4',
+                'num_epoch': 200,
+                'batch_size': 1024,
+                'optimizer': 'adam',
+                'adam_lr': 1e-3,
+                'num_users': 73515,
+                'num_items': 11200,
+                'latent_dim_mf': 32,
+                'latent_dim_mlp': 32,
+                'num_negative': 4,
+                'layers': [16,32,16,8],  # layers[0] is the concat of latent user vector & latent item vector
+                'l2_regularization': 0.01,
+                'use_cuda': True,
+                'device_id': 7,
+                'pretrain': True,
+                'pretrain_mf': 'checkpoints/{}'.format('gmf_factor8neg4_Epoch100_HR0.6391_NDCG0.2852.model'),
+                'pretrain_mlp': 'checkpoints/{}'.format('mlp_factor8neg4_Epoch100_HR0.5606_NDCG0.2463.model'),
+                'model_dir':'checkpoints/{}_Epoch{}_HR{:.4f}_NDCG{:.4f}.model'
+                }
+
+
+sample_generator = SampleGenerator(ratings=pd.read_csv('./data/rating.csv'))
+evaluate_data = sample_generator.evaluate_data
+
+config = gmf_config
+trainer = Trainer(GMF, config)
+
+for epoch in range(config['num_epoch']):
+    print('Epoch {} starts !'.format(epoch))
+    print('-' * 80)
+    train_loader = sample_generator.instance_a_train_loader(config['batch_size'])
+    trainer.train_an_epoch(train_loader, epoch_id=epoch)
+    hit_ratio, ndcg = trainer.evaluate(evaluate_data, epoch_id=epoch)
+    trainer.save(config['alias'], epoch, hit_ratio, ndcg)
